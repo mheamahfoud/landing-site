2926340d442daecc757aa5e5c1bfb1f56373e17f
WINDOWS 10 <WINDOWS 10@DESKTOP-5E1P39L>
2023-05-27 13:40:22 +0300

    commit

--- src/pages/landing/components/dubbingSerivices/ImageOverLap.tsx
+++ src/pages/landing/components/dubbingSerivices/ImageOverLap.tsx
@@ -1,4 +1,4 @@
-import {  BigImageContainer,  ImageContainer, SmallImageContainer, SmallImageOverlapContainer } from "./styles/ContainerImage";
+import {  BigImageContainer,  ImageContainer, SmallImageContainer } from "./styles/ContainerImage";
 // import BigImageSrc from '../../../../assets/images/DubbingService-Big1.jpg';
 // import SmallImageSrc from '../../../../assets/images/DubbingService-Small1.jpg';
 // SmallImageContainer, SmallImageOverlapContainer
